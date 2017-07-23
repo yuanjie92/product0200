18ac4a62fcf118fda2b4dddd17dc9f6798613c29
yuanjie92 <425697230@qq.com>
2017-07-23 13:43:58 +0800

    init

---
+++ product_0100/src/com/product/connection/ConnectionDB.java
@@ -0,0 +1,41 @@
+package com.product.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class ConnectionDB {
+	private final String URL = "jdbc:oracle:thin@192.168.0.103:1521:XE";
+	private final String NAME = "oracle";
+	private final String PASSWORD = "oracle";
+	private Connection conn;
+	
+	public ConnectionDB() {
+		try {
+			Class.forName("oracle.jdbc.OracleDriver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		try {
+			conn = DriverManager.getConnection(URL,NAME,PASSWORD);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public Connection getConnection(){
+		return conn;
+	}
+		
+	public void close(){
+		if(conn==null){
+			return;
+		}
+		try {
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ product_0100/src/com/product/model/ProductModel.java
@@ -0,0 +1,57 @@
+package com.product.model;
+
+/*商品编号
+商品类型
+商品价格
+商品数量
+商品id*/
+public class ProductModel {
+	private int code;// 商品编号
+	private String name;// 商品种类
+	private double price;// 商品价格
+	private int count;// 商品数量
+	private int id;// id
+
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public ProductModel() {
+	}
+}
